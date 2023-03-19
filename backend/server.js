09fe0c3a639ebe634c6874ff54d3f4ea40232598
Shubham Singh <singhshubhamsinghgkp@gmail.com>
2023-03-19 00:15:39 -0400

    third commit

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,6 @@ dotenv.config({path:"backend/config/config.env"})
 connectDatabase()
 
 
-
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
-
-
 const server = app.listen(process.env.PORT,()=>{
     console.log(`Server is running on http://localhost:${process.env.PORT}`)
 })
